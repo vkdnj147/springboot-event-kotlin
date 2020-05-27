3c11777647166ff30f410c03f82c516738574816
리나 <lina@linaui-MacBookPro.local>
2020-05-27 14:34:39 +0900

    주석추가

--- src/main/kotlin/com/linaexample/kotlin/event/Post/PostHandler.kt
+++ src/main/kotlin/com/linaexample/kotlin/event/Post/PostHandler.kt
@@ -11,12 +11,16 @@ import org.springframework.web.servlet.function.body
 @Component
 class PostHandler(private val repository: PostRepository, private val postService: PostService) {
 
+    //핸들러 함수는 ServerRequest 인스턴스를 승인하고 Mono <ServerResponse> 인스턴스를 생성합니다.
+
+    //.map 값을 변형해서 새로운 리스트를 생성
     fun listPost(request: ServerRequest): ServerResponse {
         val posts: List<PostView> = repository.findAll().map(this::entityToView)
         return ok().contentType(APPLICATION_JSON).body(posts);
     }
 
     fun createPost(request: ServerRequest): ServerResponse {
+        //body에 있는 내용을 저장
         val post = request.body<Post>()
         postService.savePost(post)
 //        repository.save(post)
@@ -25,9 +29,10 @@ class PostHandler(private val repository: PostRepository, private val postServic
 
     fun getPost(request: ServerRequest): ServerResponse {
         val postId = request.pathVariable("id").toLong()
+        //.let - 함수를 호출하는 객체
+        //id가 일치하면 json 타입으로 호출
         return repository.findById(postId)?.let { ok().contentType(APPLICATION_JSON).body(it) }
                 ?: ServerResponse.notFound().build()
-
     }
 
     fun entityToView(post: Post): PostView {
--- src/main/kotlin/com/linaexample/kotlin/event/Post/PostRouter.kt
+++ src/main/kotlin/com/linaexample/kotlin/event/Post/PostRouter.kt
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.function.router
 @Configuration
 class PostRouter {
 
+//WebFlux에서 라우터 기능은 RequestPredicate (즉, 요청을 관리해야하는 사람) 및 HandlerFunction
+// (즉, 요청을 구체화하는 방법)에 의해 결정되는 기능입니다.
+//
+//핸들러 함수는 ServerRequest 인스턴스를 승인하고 Mono <ServerResponse> 인스턴스를 생성합니다.
     @Bean
     fun postRoute(handler: PostHandler) = router {
         accept(APPLICATION_JSON).nest {
